07317e86d7889137899cdfca42815f2aabee53d6
hsinju0511 <debby200822024@gmail.com>
2020-04-23 03:33:17 +0800

    :pencil: second

---
+++ 5.mjpg_sample/camera_pi.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+#+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+#|R|a|s|p|b|e|r|r|y|P|i|.|c|o|m|.|t|w|
+#+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+# Copyright (c) 2014, raspberrypi.com.tw
+# All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+#
+# Author : sosorry
+# Date   : 05/31/2015
+# Origin : http://blog.miguelgrinberg.com/post/video-streaming-with-flask
+
+import cv2
+import time
+import imutils
+from imutils.video.pivideostream import PiVideoStream
+
+class Camera(object):
+    def __init__(self):
+        if cv2.__version__.startswith('2'):
+            PROP_FRAME_WIDTH = cv2.cv.CV_CAP_PROP_FRAME_WIDTH
+            PROP_FRAME_HEIGHT = cv2.cv.CV_CAP_PROP_FRAME_HEIGHT
+        elif cv2.__version__.startswith('3'):
+            PROP_FRAME_WIDTH = cv2.CAP_PROP_FRAME_WIDTH
+            PROP_FRAME_HEIGHT = cv2.CAP_PROP_FRAME_HEIGHT
+
+        self.video = PiVideoStream().start()
+        time.sleep(2.0)
+        #self.video = cv2.VideoCapture(1)
+        #self.video.set(PROP_FRAME_WIDTH, 640)
+        #self.video.set(PROP_FRAME_HEIGHT, 480)
+        self.video.set(PROP_FRAME_WIDTH, 320)
+        self.video.set(PROP_FRAME_HEIGHT, 240)
+    
+    def __del__(self):
+        self.video.release()
+    
+    def get_frame(self):
+        success, image = self.video.read()
+        ret, jpeg = cv2.imencode('.jpg', image)
+        return jpeg.tostring()
+
